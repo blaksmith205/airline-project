5c53e3d2710386da55355724fa29cfc0a79716ef
William Blanc <blanc.willjp@gmail.com>
2018-11-27 01:20:12 -0500

    Added test for file reader with reserverations.txt

---
+++ Test Packages/github/airlineproject/util/FileIOTest.java
@@ -0,0 +1,68 @@
+package github.airlineproject.util;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author William
+ */
+public class FileIOTest {
+
+    public FileIOTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of fileWriter method, of class FileIO.
+     */
+    @Test
+    public void testFileWriter() {
+        Passenger test = new Passenger("1035093287", "George", "Eric", "2a", "AA1230");
+        Passenger test2 = new Passenger("E8742784", "Tom", "Ed", "1c", "AA1140");
+        String[] lines = {FileIO.RESERVATION_HEADER, test.toFileString(),
+            test2.toFileString()};
+        FileIO.fileWriter("reservations.txt", lines);
+
+        System.out.println("Successfully wrote to reservations.txt");
+
+        ArrayList<String> flightLines = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            flightLines.add(i + "\t" + FileIO.SEAT_MAP_ROW);
+        }
+        FileIO.fileWriter("AA1230.txt", flightLines.toArray(new String[flightLines.size()]));
+        System.out.println("Successfully created AA1230 seat map");
+    }
+
+    /**
+     * Test of fileAppender method, of class FileIO.
+     */
+    @Test
+    public void testFileAppender() {
+    }
+
+    /**
+     * Test of fileReader method, of class FileIO.
+     */
+    @Test
+    public void testFileReader() {
+        ArrayList<String> lines = FileIO.fileReader("reservations.txt");
+        ArrayList<Passenger> passengers = new ArrayList<>();
+        lines.remove(0);    // Remove header
+        for (String line : lines) {
+            String[] passengerInfo = line.split("\t");
+            passengers.add(new Passenger(passengerInfo[0].trim(), passengerInfo[1].trim(),
+                    passengerInfo[2].trim(), passengerInfo[3].trim()));
+        }
+        
+        for(Passenger p: passengers){
+            System.out.println(p);
+        }
+    }
+
+}
